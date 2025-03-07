6530a2b09d4d3aaf15a87994045799edf3935086
eduardobandeiramg <eduardobandeiramg@gmail.com>
2025-03-07 13:47:49 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto flutter para aprender gerenciamento de estados
---
+++ android/app/src/main/kotlin/com/eduardobandeiramg/aprendendo_gerenciamento_estados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.eduardobandeiramg.aprendendo_gerenciamento_estados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
